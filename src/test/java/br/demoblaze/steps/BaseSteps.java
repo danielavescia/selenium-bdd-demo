dd6c3140fdd27d522eb6b7b14c821190ed47cec5
danivescia <danivescia@gmail.com>
2026-01-15 15:50:19 -0300

    chore: implement CI pipeline

--- src/test/java/br/demoblaze/steps/BaseSteps.java
+++ src/test/java/br/demoblaze/steps/BaseSteps.java
@@ -1,14 +1,32 @@
 package br.demoblaze.steps;
 
+import java.time.Duration;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseSteps {
     
     protected static WebDriver driver;
 
     public static void openBrowser(){
-        driver = new ChromeDriver();
+        WebDriverManager.chromedriver().setup();
+    
+        ChromeOptions options = new ChromeOptions();
+
+        boolean isPipelineCI = System.getenv("CI") != null;
+
+        if(isPipelineCI){
+            options.addArguments("--headless=new");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+        }
+
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         driver.manage().window().maximize();
     }
 
--- src/test/resources/feature/DemoBlazeSignUp.feature
+++ src/test/resources/feature/DemoBlazeSignUp.feature
@@ -5,6 +5,6 @@ Feature: Register on DemoBlaze
     Scenario: Sucessful registration
         Given I'm on the DemoBlaze home page
         When I click on Sign up button
-        And I fill out the form with "email1@test.com", "123"
+        And I fill out the form with "email123@test.com", "123"
         And I click on the Sign up button
         Then I see the registration message "Sign up successful."
